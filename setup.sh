064041fd9837d5aaa8fd61e52f6150a6e2b20038
Christian Bunn <ctbunn333@gmail.com>
2024-09-29 17:07:49 +1000

    added docker compose

--- setup.sh
+++ setup.sh
@@ -1,9 +1,2 @@
 sudo snap install docker
 sudo chown $USER /var/run/docker.sock
-cd backend 
-docker build -t backend .
-cd ../frontend
-docker build -t frontend .
-cd ..
-docker run -d -p 3000:3000 -e  backend
-docker run -d -p 80:80 frontend
