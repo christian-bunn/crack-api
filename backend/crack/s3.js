25334044a9a50c11fc16bf5090c92260b274f095
Christian Bunn <ctbunn333@gmail.com>
2024-09-29 22:24:01 +1000

    implemented the use of elaticache

--- backend/crack/s3.js
+++ backend/crack/s3.js
@@ -9,7 +9,8 @@ const {
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const S3Presigner = require("@aws-sdk/s3-request-presigner");
 const bucketName = "n11092505-assessment-2";
-
+const Memcached = require('memcached');
+const memcached = new Memcached('n11092505-cache.km2jzi.0001.apse2.cache.amazonaws.com:11211');
 let s3Client = new S3.S3Client({ region: "ap-southeast-2" });
 
 (async () => {
@@ -39,7 +40,6 @@ let s3Client = new S3.S3Client({ region: "ap-southeast-2" });
   }
 })();
 
-const cache = {};
 
 // note: store the files in s3 based on username ("sub": "d9ae4408-f031-7010-d902-7422e61d7ad2") - sub means subject and is the id of the user
 // note: make sure public access to the s3 is disabled (block all public access)
@@ -70,22 +70,26 @@ const uploadFile = async (folder, fileName, contentType) => {
 // download file (file id) returns (signed url)
 // provide endpoint to download file from s3, send signed url to front end
 // when the user requests a given {file id}, generate a signed url for the file at {user sub}/{file id}
+const { promisify } = require('util');
+const memcachedGet = promisify(memcached.get).bind(memcached);
+const memcachedSet = promisify(memcached.set).bind(memcached);
 const downloadFile = async (folder, fileName) => {
   const cacheKey = `${folder}/${fileName}`;
-  const currentTime = Date.now();
 
-  // Check if signed URL is in cache and hasn't expired
-  if (cache[cacheKey]) {
-    if (cache[cacheKey].expiry > currentTime) {
-      console.log("Returning cached presigned URL for:", cacheKey);
-      return cache[cacheKey].url;
-    } else {
-      // Remove expired URL from cache
-      delete cache[cacheKey];
+  // this checks if the signed url is in and and that is hasn't expired
+  try {
+    const dataStr = await memcachedGet(cacheKey);
+    if (dataStr) {
+      const data = JSON.parse(dataStr);
+      console.log('Returning cached presigned URL for:', cacheKey);
+      return data.url;
     }
+  } catch (err) {
+    console.error('Error getting from memcached:', err);
+    // proceed to generate a new url
   }
 
-  // Create a pre-signed URL for getting an object
+  // this trys to create a pre-signed URL for getting an object from s3
   try {
     const command = new GetObjectCommand({
       Bucket: bucketName,
@@ -94,19 +98,21 @@ const downloadFile = async (folder, fileName) => {
     });
     const expiresIn = 3600; // 1 hour
     const presignedURL = await getSignedUrl(s3Client, command, { expiresIn });
-    console.log("Pre-signed URL to get the object:");
+    console.log('Pre-signed URL to get the object:');
     console.log(presignedURL);
 
-    // Store the URL and its expiry time in cache
-    cache[cacheKey] = {
-      url: presignedURL,
-      expiry: currentTime + expiresIn * 1000, // expiry time in milliseconds
-    };
+    // storing the url and expiry in cache
+    try {
+      await memcachedSet(cacheKey, JSON.stringify({ url: presignedURL }), expiresIn);
+    } catch (err) {
+      console.error('Error setting to memcached:', err);
+      // even if setting to cache failed, the url can still be returned
+    }
 
     return presignedURL;
   } catch (err) {
     console.log(err);
-    throw err; // Re-throw the error so the caller can handle it
+    throw err;
   }
 };
 
