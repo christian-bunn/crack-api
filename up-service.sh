af19b6c104ea188274a626ed0a27abaf34e2b150
Christian Bunn <ctbunn333@gmail.com>
2024-11-03 19:30:47 +1000

    updated to use 3 separate services

--- up-service.sh
+++ up-service.sh
@@ -1,5 +1,28 @@
+cd frontend
 ecs-cli compose \
+    --project-name 'frontend' \
+    --ecs-params 'ecs-params.yml' \
+    service up \
+    --tags 'qut-username=n11092505@qut.edu.au,purpose=assessment3' \
+    --launch-type FARGATE \
+    --enable-service-discovery
+cd -
+
+cd backend
+ecs-cli compose \
+    --project-name 'backend' \
+    --ecs-params 'ecs-params.yml' \
+    service up \
+    --tags 'qut-username=n11092505@qut.edu.au,purpose=assessment3' \
+    --launch-type FARGATE \
+    --enable-service-discovery
+cd -
+
+cd cracker
+ecs-cli compose \
+    --project-name 'cracker' \
     --ecs-params 'ecs-params.yml' \
     service up \
     --tags 'qut-username=n11092505@qut.edu.au,purpose=assessment3' \
     --launch-type FARGATE
+cd -
